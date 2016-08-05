acbf827d36d5ef125e74f50e5029b73de6ca6456
Yvette <libra1014tyb@163.com>
2016-08-05 21:50:32 +0800

    component-demo

---
+++ component.js
@@ -0,0 +1,10 @@
+class Example extends React.Component{
+    render(){
+        return <h1>Hello World</h1>
+    }
+}
+
+ReactDOM.render(
+    <Example/>,
+    document.getElementById('example')
+);
